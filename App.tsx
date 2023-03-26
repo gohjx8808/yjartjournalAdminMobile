b37891b629633410708e9bc1a72e1a04aca97fe1
Jing Xuan Goh <gohjx8808@gmail.com>
2023-03-26 21:46:35 +0800

    integrated react-native-elements

--- App.tsx
+++ App.tsx
@@ -1,11 +1,14 @@
 import React from "react";
 import {NavigationContainer} from "@react-navigation/native";
 import MainRouter from "./src/modules/router/MainRouter";
+import {SafeAreaProvider} from "react-native-safe-area-context";
 
 const App = (): JSX.Element => (
-	<NavigationContainer>
-		<MainRouter />
-	</NavigationContainer>
+	<SafeAreaProvider>
+		<NavigationContainer>
+			<MainRouter />
+		</NavigationContainer>
+	</SafeAreaProvider>
 );
 
 export default App;
