999dd6be6fd13806a6bfd0bfdb20574beb7cab0f
Jing Xuan Goh <gohjx8808@gmail.com>
2023-03-26 22:23:31 +0800

    rename to yarn stocks

--- .prettierrc.js
+++ .prettierrc.js
@@ -1,10 +1,8 @@
 module.exports = {
 	arrowParens: "avoid",
 	bracketSameLine: true,
-	bracketSpacing: false,
 	trailingComma: "all",
 	semi: true,
 	singleQuote: false,
-	printWidth: 80,
 	useTabs: true,
 };
