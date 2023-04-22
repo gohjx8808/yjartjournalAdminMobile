f64dbdd0125b0c54a02261daa9c0ba46c4cda2fe
Jing Xuan Goh <gohjx8808@gmail.com>
2023-04-22 16:19:40 +0800

    move yarn stock common input to individual file

--- src/modules/yarnStock/views/AddYarnStock.tsx
+++ src/modules/yarnStock/views/AddYarnStock.tsx
@@ -4,15 +4,9 @@ import type { SubmitHandler } from "react-hook-form";
 import { useForm } from "react-hook-form";
 import { convertUTCToMYT } from "../../../helpers/helpers";
 import YJHeader from "../../../layout/YJHeader";
-import ControlledDatePicker from "../../../sharedComponents/inputs/ControlledDatePicker";
-import ControlledSelect from "../../../sharedComponents/inputs/ControlledSelect";
-import ControlledTextInput from "../../../sharedComponents/inputs/ControlledTextInput";
 import AddYarnStockSchema from "../../../validationSchemas/AddYarnStockSchema";
-import {
-	useAllYarnCategories,
-	useAllYarnColorCategories,
-} from "../../masterData/src/queries/masterDataQueries";
 import { useAddYarnStock } from "../src/queries/yarnStockMutations";
+import YarnStockSharedInputs from "./YarnStockSharedInputs";
 
 const AddYarnStock = () => {
 	const styles = useStyles();
@@ -25,8 +19,6 @@ const AddYarnStock = () => {
 		resolver: yupResolver(AddYarnStockSchema),
 	});
 
-	const { data: yarnCategories } = useAllYarnCategories();
-	const { data: yarnColorCategories } = useAllYarnColorCategories();
 	const { mutate: addYarnStock } = useAddYarnStock();
 
 	const onSubmit: SubmitHandler<yarnStock.addYarnStockPayload> = formData => {
@@ -42,52 +34,7 @@ const AddYarnStock = () => {
 			back
 			customScrollViewContentContainerStyle={styles.scrollViewContent}
 		>
-			<ControlledTextInput
-				control={control}
-				name="detailedColor"
-				label="Detailed Color"
-				errorMessage={errors.detailedColor?.message?.toString()}
-			/>
-			<ControlledTextInput
-				control={control}
-				keyboardType="numeric"
-				name="cost"
-				label="Cost"
-				errorMessage={errors.cost?.message?.toString()}
-			/>
-			<ControlledTextInput
-				control={control}
-				keyboardType="numeric"
-				name="quantity"
-				label="Quantity"
-				errorMessage={errors.quantity?.message?.toString()}
-			/>
-			<ControlledTextInput
-				control={control}
-				keyboardType="numeric"
-				name="reorderLevel"
-				label="Reorder Level"
-				errorMessage={errors.reorderLevel?.message?.toString()}
-			/>
-			<ControlledSelect
-				control={control}
-				name="yarnCategory"
-				title="Yarn Category"
-				options={yarnCategories ?? []}
-				errorMessage={errors.yarnCategory?.id?.message?.toString()}
-			/>
-			<ControlledSelect
-				control={control}
-				name="yarnColorCategory"
-				title="Yarn Color Category"
-				options={yarnColorCategories ?? []}
-				errorMessage={errors.yarnColorCategory?.id?.message?.toString()}
-			/>
-			<ControlledDatePicker
-				control={control}
-				name="lastOrderedDate"
-				title="Last Ordered Date"
-			/>
+			<YarnStockSharedInputs control={control} errors={errors} />
 			<Button
 				color="secondary"
 				containerStyle={styles.submitBtnContainer}
